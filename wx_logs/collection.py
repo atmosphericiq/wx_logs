84e47112c7d2c114d1d8c257fc9562b988036f41
Tom Hayden <thayden@gmail.com>
2024-08-08 09:50:11 -0500

    fixing imports

--- wx_logs/collection.py
+++ wx_logs/collection.py
@@ -1,7 +1,10 @@
 import json
 import logging
 import datetime
-from wx_logs import WeatherStation
+from .weather_station import WeatherStation
+from .kriger import Kriger
+from .raster_band import RasterBand
+from .file_storage import FileStorage
 
 logger = logging.getLogger(__name__)
 
@@ -20,13 +23,24 @@ class Collection:
     station_id = str(station_id)
     self._stations[station_id] = station
 
+  # append elevation will combine DEM elevation rasters
+  # with the station data, to make sure we can set the right
+  # elevation on all the points
+  def append_elevation_data(self):
+    pass
+
+  # this will take a single variable on all stations
+  # and krige it over some period of time
+  def krige_single_variable(self, variable_name):
+    pass
+
   def new_station(self, station_type, station_id):
     station_id = str(station_id)
     s = WeatherStation(station_type)
     s.set_station_id(station_id)
     self.add_station(s)
     return s
-  
+
   def num_stations(self):
     return len(self._stations)
 
